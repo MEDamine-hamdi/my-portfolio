015cfd6d4a831b9aa092e0f48d9a9d71c3d7494a
MEDamine-hamdi <hamdiamine779@gmail.com>
2024-11-25 09:51:20 +0100

    test 3

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -7,12 +7,15 @@ import py from '../../../public/py.png';
 import c from '../../../public/c.png';
 import java from '../../../public/java.png';
 
+import sql from '../../../public/sql.png';
+import mongo from '../../../public/mongo.png';
+
 import Image from "next/image"
 
 export default function About () {
     return (
         <section className="w-full h-[80vh] p-20" id="about">
-            <h1 className='font-bold text-2xl pb-4'>skils</h1>
+            <h1 className='font-bold text-2xl pb-4 text-blue-600'>skills</h1>
             <div className='flex flex-row item-center pb-8'>
                 <Image src={js} alt="logo" className='h-20 w-auto mr-2' />
                 <Image src={react} alt="logo" className='h-20 w-auto mr-2' />
@@ -20,10 +23,14 @@ export default function About () {
                 <Image src={exp} alt="logo" className='h-20 w-auto mr-3'  />
                 <Image src={php} alt="logo" className='h-20 w-auto mr-3'  />
             </div>
-            <div className='flex flex-row item-center'>
+            <div className='flex flex-row item-center pb-8'>
                 <Image src={py} alt="logo" className='h-20 w-auto mr-2' />
                 <Image src={c} alt="logo" className='h-20 w-auto mr-2' />
                 <Image src={java} alt="logo" className="h-20 w-auto bg-slate-300 rounded-xl mr-3"/>
+            </div>
+            <div className='flex flex-row item-center pb-8'>
+                <Image src={mongo} alt="logo" className='h-20 w-auto mr-2' />
+                <Image src={sql} alt="logo" className='h-20 w-auto mr-2' />
             </div>
         </section>
     )
